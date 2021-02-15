973f509b21b90771305d1cb6777a3e22b9b7485e
chiaracc <chiaracaldinelli@gmail.com>
2021-02-15 23:30:47 +0000

    removed /

--- chiara_hcp_roi.py
+++ chiara_hcp_roi.py
@@ -33,7 +33,7 @@ for sub in subjNums:
         # This was running:
         #obj = s3.download_file('hcp-openaccess', 'HCP_1200/424939/MNINonLinear/Results/tfMRI_MOTOR/tfMRI_MOTOR_hp200_s2_level2.feat/424939_tfMRI_MOTOR_level2_hp200_s2.dscalar.nii','testme.nii')
         print(f'/HCP_1200/{sub}/MNINonLinear/Results/{con}/{con}_hp200_s2_level2.feat/{sub}_{con}_level2_hp200_s2.dscalar.nii')        
-        obj = s3.download_file('hcp-openaccess', f'/HCP_1200/{sub}/MNINonLinear/Results/{con}/{con}_hp200_s2_level2.feat/{sub}_{con}_level2_hp200_s2.dscalar.nii', 'testme.nii')
+        obj = s3.download_file('hcp-openaccess', f'HCP_1200/{sub}/MNINonLinear/Results/{con}/{con}_hp200_s2_level2.feat/{sub}_{con}_level2_hp200_s2.dscalar.nii', 'testme.nii')
 
 #obj = s3.Object('s3://hcp-openaccess/HCP_1200/{sub}/MNINonLinear/Results/{con}/{con}_hp200_s2_level2.feat/{sub}_{con}_level2_hp200_s2.dscalar.nii', 'objname')  
         # Checking the previous command ran OK
