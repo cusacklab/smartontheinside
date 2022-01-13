0203de40e1517a0e6abe0bb23735128c1ab8898b
chiaracc <chiaracaldinelli@gmail.com>
2022-01-13 12:15:28 +0000

    Update transformROIs.sh

--- transformROIs.sh
+++ transformROIs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dir=/home/chiaracaldinelli/smartontheinside/smartontheinside/
+dir=/home/chiaracaldinelli/smartonetheinside/smartontheinside
 
 
 for roi in {1..180}; do
-    wb_command -label-to-volume-mapping ${dir}ROIs/ROI.${roi}.L.label.gii ${dir}Q1-Q6_RelatedParcellation210.L.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}mean_Rsamples.nii.gz /dhcp/smartontheinside/ROIs/ROI.${roi}.L.nii -nearest-vertex 1
+    wb_command -label-to-volume-mapping ${dir}/ROIs/ROI.${roi}.L.label.gii ${dir}/Q1-Q6_RelatedParcellation210.L.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}/mean_Rsamples.nii.gz /dhcp/smartontheinside/smartontheinside/ROIs/ROI.${roi}.L.nii -nearest-vertex 1
 done
