4829885c1ae5b5a798c9ae3c64397caaea2e041f
chiaracc <chiaracaldinelli@gmail.com>
2022-01-13 12:05:49 +0000

    Update transformROIs.sh

--- transformROIs.sh
+++ transformROIs.sh
@@ -4,5 +4,5 @@ dir=/home/chiaracaldinelli/smartontheinside/
 
 
 for roi in {1..360}; do
-    wb_command -label-to-volume-mapping ${dir}ROIs/ROI.${roi}.L.label.gii ${dir}Q1-Q6_RelatedParcellation210.L.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}mean_Rsamples.nii.gz ${dir}ROIs/ROI.${roi}.L.nii -nearest-vertex 1
+    wb_command -label-to-volume-mapping ${dir}ROIs/ROI.${roi}.L.label.gii ${dir}Q1-Q6_RelatedParcellation210.L.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}mean_Rsamples.nii.gz /dhcp/smartontheinside/ROIs/ROI.${roi}.L.nii -nearest-vertex 1
 done
