9569063e47d30faae6a4cd373d275144ca5133b8
chiaracc <chiaracaldinelli@gmail.com>
2022-01-18 11:14:46 +0000

    Update tractography.sh

--- tractography.sh
+++ tractography.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+# Chiara Caldinelli, caldinec@tcd.ie
+# Cusack Lab, Trinity College Dublin
+
+# This code will run tractography from the DLPF area to every single ROI outside it. This will be run with a subgroup of the HCP 1200 subjects.
 # This code will:
-# 1- Create tmp folder
-# 2- Sync aws
-# 3- ROI.gii → .nii
-# 4- Tractography
+#   1- Create a tmp folder
+#   2- Sync aws: retrieve native data used for step 3 from S3, in a temporary folder
+#   3- ROI.gii → ROI.nii
+#   4- Tractography
+
+
+dir=/home/chiaracaldinelli/smartonetheinside/smartontheinside
+
 
 # 1- Create tmp folder
 tmp_dir=$(mktemp -d -t chiara-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
 
 
 # 2- Sync aws
-aws s3 cp --profile hcp s3://hcp-openaccess/HCP_1200/996782/T1w/T1w_acpc_dc.nii.gz T1w_acpc_dc.nii.gz
-aws s3 sync --profile hcp s3://hcp-openaccess/HCP_1200/996782/T1w/Native 996782/T1w/Native
-aws s3 sync --profile hcp s3://hcp-openaccess/HCP_1200/996782/T1w/Diffusion.bedpostX 996782/T1w/Diffusion.bedpostX
+aws s3 sync --profile hcp s3://hcp-openaccess/HCP_1200/${subj}/T1w/Native ${tmp_dir}/
+aws s3 sync --profile hcp s3://hcp-openaccess/HCP_1200/${subj}/T1w/Diffusion.bedpostX ${tmp_dir}/
 
 
 # 3- ROI.gii → .nii
-dir=/home/chiaracaldinelli/smartonetheinside/smartontheinside
+
 for roi in {1..180}; do
     wb_command -label-to-volume-mapping ${dir}/ROIs/ROI.${roi}.L.label.gii ${dir}/Q1-Q6_RelatedParcellation210.L.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}/mean_Rsamples.nii.gz /dhcp/smartontheinside/smartontheinside/ROIs/ROI.${roi}.L.nii -nearest-vertex 1
-done
 
 
 # 4- Tractography
-/usr/local/fsl/bin/probtrackx2 -x /Users/chiara/smartontheinside/frontal.L.nii -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s /Users/chiara/smartontheinside/./merged -m /Users/chiara/smartontheinside/./nodif_brain_mask --dir=/Users/chiara/smartontheinside/tractography-ouput
+    for hem in {'L', 'R'}; do
+        /usr/local/fsl/bin/probtrackx2 -x /Users/chiara/smartontheinside/fronta${hem}.L.nii -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s /Users/chiara/smartontheinside/./merged -m /Users/chiara/smartontheinside/./nodif_brain_mask --dir=/Users/chiara/smartontheinside/tractography-ouput
+    done
+done
