47e3a0c27cc9a4c13bb773f57b4b68bf5b2ffff0
chiaracc <chiaracaldinelli@gmail.com>
2022-01-20 15:25:48 +0000

    new nii roi

--- transformROIs.sh
+++ transformROIs.sh
@@ -4,5 +4,5 @@ dir=/home/chiaracaldinelli/smartonetheinside/smartontheinside
 
 
 for roi in {1..180}; do
-    wb_command -label-to-volume-mapping ${dir}/ROIs/ROI.${roi}.R.label.gii ${dir}/Q1-Q6_RelatedParcellation210.R.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}/mean_Rsamples.nii.gz /dhcp/smartontheinside/smartontheinside/ROIs/ROI.${roi}.L.nii -nearest-vertex 1
+    wb_command -label-to-volume-mapping ${dir}/ROIs/ROI.${roi}.R.label.gii ${dir}/Q1-Q6_RelatedParcellation210.R.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}/mean_Rsamples.nii.gz /dhcp/smartontheinside/smartontheinside/ROIs/ROI.${roi}.nii -nearest-vertex 1
 done
