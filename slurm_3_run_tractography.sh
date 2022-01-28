2b41bcc7fc4f44e70694386f35e0763a8fed9fbd
chiaracc <chiaracaldinelli@gmail.com>
2022-01-28 12:00:34 +0000

    Update slurm_3_run_tractography.sh

--- slurm_3_run_tractography.sh
+++ slurm_3_run_tractography.sh
@@ -29,6 +29,7 @@ aws s3 sync s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/Diffusion.bedpostX ${tmp_dir}
 # DLPFC mask
 for hem in {'L', 'R'}; do
     wb_command -label-to-volume-mapping /dhcp/smartontheinside/smartontheinside/frontal.${hem}.label.gii /home/chiaracaldinelli/smartontheinside/smartontheinside/Q1-Q6_RelatedParcellation210.${hem}.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${dir}/T1w_acpc_dc.nii.gz ${dir}/frontal.${hem}.nii -nearest-vertex 1
+    done
 # ROIs
 mkdir ${tmp_dir}/ROIs/
 for roi in {1..180}; do
