aa8343d554e20857196da066fbe97a2cde393115
chiaracc <chiaracaldinelli@gmail.com>
2022-01-28 11:41:20 +0000

    Update slurm_3_run_tractography.sh

--- slurm_3_run_tractography.sh
+++ slurm_3_run_tractography.sh
@@ -22,7 +22,7 @@ tmp_dir=$(mktemp -d -t chiara-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
 
 # 2- Sync aws
 aws s3 sync s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/T1w_acpc_dc.nii.gz ${tmp_dir}/ --profile hcp
-aws s3 sync s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/Diffusion.bedpostX ${tmp_dir}/ --profile hcp 
+aws s3 sync s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/Diffusion.bedpostX ${tmp_dir}/Diffusion.bedpostX/ --profile hcp
 
 
 # 3- ROI.gii → .nii
@@ -39,33 +39,14 @@ for r in {181..360}; do
 ls >> ${tmp_dir}/allROIs.txt
 
 # 4- Tractography
-
-    /usr/local/fsl/bin/probtrackx2 -x ${tmp_dir}/frontal.${hem}.nii -l --onewaycondition -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --targetmasks /Users/chiara/smartontheinside/ROIs/allROIs.txt --forcedir --opd -s ${tmp_dir}/Diffusion.bedpostX/merged -m ${tmp_dir}/Diffusion.bedpostX/nodif_brain_mask --dir=/dhcp/smartontheiside/smartontheiside/output_tract_${q}
-done
-
-
-
-SUBJ=$1
-SES=$2
-DWIPTH=/dhcp/dhcp_dmri_pipeline
-TOSPLIT=${DWIPTH}/${SUBJ}/${SES}/probtrackx2/${SUBJ}_${SES}_hcp_regions_v4_targe
-t_dwi
-TEXTOUT=${TOSPLIT}_list.txt
-
-DONEFILE=${DWIPTH}/${SUBJ}/${SES}/probtrackx2/done_probtrackx2
-
-rm $DONEFILE
-
 for hem in {'L', 'R'}; do
     probtrackx2_gpu --onewaycondition -P 5000 --forcedir --opd --os2t \
-	    --rseed=1234 -s ${DWIPTH}/${SUBJ}/${SES}/dwi.bedpostX/merged \
-	    --dir=${DWIPTH}/${SUBJ}/${SES}/probtrackx2 \
-	    -m ${DWIPTH}/${SUBJ}/${SES}/dwi/${SUBJ}_${SES}_desc-preproc_space-dwi_br
-    ainmask.nii.gz  \
-	    --targetmasks=$TEXTOUT  \
-	    -x ${DWIPTH}/${SUBJ}/${SES}/probtrackx2/${SUBJ}_${SES}_hcp_regions_v4_se
-    ed_dwi.nii.gz \
-        -o fdt_paths_hcp_rhodri
+	    --rseed=1234 -s ${tmp_dir}/Diffusion.bedpostX/merged \
+	    --dir=${tmp_dir}/probtrackx2 \
+	    -m ${tmp_dir}/Diffusion.bedpostX/merged/nodif_brain_mask.nii.gz  \
+	    --targetmasks=${tmp_dir}/allROIs.txt  \
+	    -x ${tmp_dir}/frontal.${hem}.nii \
+        -o ${tmp_dir}/probtrackx2_res
 
-touch $DONEFILE
-
+# Push results to S3
+ aws s3 sync ${tmp_dir}/probtrackx2_res/ s3://smartontheinside/HCP_1200/$SUBJ/T1w/Diffusion.probtrackx2/
