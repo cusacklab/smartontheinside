ea12c938e278d5c4797257112e08de0694e4701e
chiaracc <chiaracaldinelli@gmail.com>
2022-02-07 10:34:32 +0000

    debug

--- slurm_3_run_tractography.sh
+++ slurm_3_run_tractography.sh
@@ -21,13 +21,12 @@ tmp_dir=$(mktemp -d -t chiara-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
 
 
 # 2- Sync aws
-aws s3 sync s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/T1w_acpc_dc.nii.gz ${tmp_dir}/ --profile hcp
 aws s3 sync s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/Diffusion.bedpostX ${tmp_dir}/Diffusion.bedpostX/ --profile hcp
-
+aws s3 cp s3://hcp-openaccess/HCP_1200/$SUBJ/T1w/T1w_acpc_dc.nii.gz ${tmp_dir}/ --profile hcp
 
 # 3- ROI.gii → .nii
 # DLPFC mask
-for hem in {'L', 'R'}; do
+for hem in 'L', 'R'; do
     wb_command -label-to-volume-mapping /dhcp/smartontheinside/smartontheinside/frontal.${hem}.label.gii /dhcp/smartontheinside/git/smartontheinside/smartontheinside/Q1-Q6_RelatedParcellation210.${hem}.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${tmp_dir}/T1w_acpc_dc.nii.gz ${dir}/frontal.${hem}.nii -nearest-vertex 1
     done
 # ROIs
@@ -38,6 +37,7 @@ for roi in {1..15}; do
 #for r in {181..360}; do
 #    wb_command -label-to-volume-mapping /dhcp/smartontheinside/git/smartontheinside/smartontheinside/ROI.${roi}.L.label.gii /dhcp/smartontheinside/git/smartontheinside/smartontheinside/Q1-Q6_RelatedParcellation210.L.midthickness_MSMAll_2_d41_WRN_DeDrift.32k_fs_LR.surf.gii ${tmp_dir}/T1w_acpc_dc.nii.gz ${tmp_dir}/ROIs/ROI.${roi}.nii -nearest-vertex 1
 #    done
+cd ${tmp_dir}/ROIs/
 ls > ${tmp_dir}/ROIs/allROIs.txt
 
 # 4- Tractography
