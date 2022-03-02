4f8e6a5cd11e18162e1010c638700592259806bc
chiaracc <chiaracaldinelli@gmail.com>
2022-03-02 13:51:26 +0000

    new cluster address

--- slurm_2_1job_tractography.sh
+++ slurm_2_1job_tractography.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J SOTI-CC
 #SBATCH --cpus-per-task=8
-#SBATCH --output=/dhcp/smartonetheinside/smartontheinside/slurm/slurm-%j.out
-#SBATCH --error=/dhcp/chiaracaldinelli/smartonetheinside/smartontheinside/slurm/slurm-%j.err
+#SBATCH --output=/home/chiaracaldinelli/slurm-%j.out
+#SBATCH --error=/home/chiaracaldinelli/smartontheinside/slurm-%j.err
 
 
 echo "In slurm_3_run_tractography.sh parameter SUBJ=$SUBJ"
-/home/chiaracaldinelli/smartonetheinside/smartontheinside/slurm_run_tractography.sh $SUBJ
+/home/chiaracaldinelli/smartontheinside/slurm_run_tractography.sh $SUBJ
