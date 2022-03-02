7d0c957afb7fdcab21b37797e4277e84f2564e56
chiaracc <chiaracaldinelli@gmail.com>
2022-03-02 14:10:20 +0000

    Update slurm_2_1job_tractography.sh

--- slurm_2_1job_tractography.sh
+++ slurm_2_1job_tractography.sh
@@ -6,4 +6,4 @@
 
 
 echo "In slurm_3_run_tractography.sh parameter SUBJ=$SUBJ"
-/home/chiaracaldinelli/smartontheinside/slurm_run_tractography.sh $SUBJ
+/home/chiaracaldinelli/smartontheinside/slurm_3_run_tractography.sh $SUBJ
