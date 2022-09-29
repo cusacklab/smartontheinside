bbf29b650629623553f813913f3ad980e30aebf6
chiaracc <chiaracaldinelli@gmail.com>
2022-09-29 15:30:42 +0100

    running classifier

--- classifier.py
+++ classifier.py
@@ -32,7 +32,7 @@ from numpy import absolute
 
 
 ### POOL SUBJECTS
-subjlist = ['106521'] # cancella questo perche e doppio
+subjlist = ['123521', '942658'] # cancella questo perche e doppio
 #subjlist = ['103818', '178950','189450','199453','209228','220721','298455','356948','419239','499566','561444','618952','680452','757764','841349','908860', '103818','113922','121618','130619','137229','151829','158035','171633','179346','190031','200008','210112','221319','299154','361234', '424939','500222','570243','622236','687163','769064','845458','911849','104416','114217','122317','130720','137532','151930','159744', '172029','180230','191235','200614','211316','228434','300618','361941','432332','513130','571144','623844','692964','773257','857263', '926862','122822','130821','137633','152427','160123','172938','180432','192035','200917','239944','303119', '365343','436239','513736','579665','638049','702133','774663','865363','930449','106521','114823','123521','130922','137936','152831', '160729','173334','180533','192136','201111','211619','249947','305830','366042','436845','516742','580650','645450','715041','782561', '871762','942658','106824','117021','123925','131823','138332','153025','162026','173536','180735','192439','201414','211821','251833', '310621','371843','445543','519950','580751','647858','720337','800941','871964','955465','107018','117122','125222','132017','138837', '153227','162329','173637','180937','193239','201818','211922','257542','314225','378857','454140', '523032', '585862','654350','725751', '803240','872562','959574','107422','117324','125424','133827','142828','153631','164030','173940','182739','194140','202719','212015', '257845','316633','381543','459453','525541','586460','654754','727553','812746','873968','966975']
 #'105014', '114419', 
 nsub=len(subjlist)
@@ -58,28 +58,32 @@ s3 = session.client('s3')
 
 
 
-############ Load connectivity data
+################################################
+############ Load connectivity data ############
+################################################
 
 for hemiind, hemi in enumerate(['R','L']):
+    conn_for_classifier = np.zeros((nsub,786600))
     
-    conn = []
-    for sub in range(nsub):
-        print(f'Working on subject {subjlist[sub]} connectivity data, {hemi} hemisphere')
-        
-        remotepath = (f'HCP_1200/{subjlist[sub]}/T1w/Diffusion.probtrackx2/{subjlist[sub]}_tractography_results_VOXEL_R.npy')
-        s3.download_file('smartontheinside', remotepath, f'/Users/chiara/{subjlist[sub]}_tractography_results_VOXEL_R.npy')
-        tract = np.load(f'/Users/chiara/{subjlist[sub]}_tractography_results_VOXEL_R.npy', allow_pickle=True)
 
-        aj = np.load(f'/Users/chiara/{subjlist[sub]}_tractography_results_VOXEL_R.npy', allow_pickle=True)
+    for subind, sub in enumerate(subjlist):
+        conn = []
+        print(f'Working on subject {sub} connectivity data, {hemi} hemisphere')
+        remotepath = (f'HCP_1200/{sub}/T1w/Diffusion.probtrackx2/{sub}_tractography_results_VOXEL_R.npy')
+        s3.download_file('smartontheinside', remotepath, f'/Users/chiara/{sub}_tractography_results_VOXEL_R.npy')
+        tract = np.load(f'/Users/chiara/{sub}_tractography_results_VOXEL_R.npy', allow_pickle=True)
+
         for target_roi in range(1, 361): # For every target ROI (334 in total)
             conn.extend(tract[target_roi, :])
         print(len(conn))
+        conn_for_classifier[subind,:] = conn
+    np.save(f'/Users/chiara/conn_for_classifier_{hemi}.npy', conn_for_classifier)
 
-    np.save(f'/Users/chiara/conn_for_classifier_{hemi}.npy', conn)
 
 
-
-############ Load activity data
+############################################
+############ Load activity data ############
+############################################
 
 for task, taskcons in taskcondict_selected.items():
     for subind, sub in enumerate(subjlist):
@@ -105,18 +109,18 @@ for task, taskcons in taskcondict_selected.items():
 
 
 
-
-
+####################################
 ############ CLASSIFIER ############
+####################################
 
 # act and conn_right conn_left
 
 # define model
 model = ElasticNet(alpha=1.0, l1_ratio=0.5)
 # define model evaluation method
-cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+cv = RepeatedKFold(n_splits=2, n_repeats=3, random_state=1)
 # evaluate model
-scores = cross_val_score(model, act_for_classifier, conn, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
+scores = cross_val_score(model, act_for_classifier, conn_for_classifier, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
 # force scores to be positive
 scores = absolute(scores)
 print('Mean MAE: %.3f (%.3f)' % (mean(scores), std(scores)))
