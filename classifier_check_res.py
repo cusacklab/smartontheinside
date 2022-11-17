67f4b094e6b44df8f5c3c229123f1c851237cbe5
chiaracc <chiaracaldinelli@gmail.com>
2022-11-17 14:24:30 +0000

    news

--- classifier_check_res.py
+++ classifier_check_res.py
@@ -2,11 +2,16 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from statsmodels.graphics.factorplots import interaction_plot
+import numpy as np
+
+
 
 alpha_values = [0.2, 0.4, 0.8, 1.0]
 l1_ratio_values = [0.0, 0.2, 0.4, 0.8, 1.0]
 
-analysis_root = '/home/chiaracaldinelli'
+analysis_root = '/User/chiara/smartontheinside'
+
+m = np.zeros((len(alpha_values),len(l1_ratio_values)))
 
 for alpha in alpha_values:
     for l1_ratio in l1_ratio_values:
@@ -15,6 +20,7 @@ for alpha in alpha_values:
 
         dfL = df.loc[df['hemi'] == 'L']
         dfR = df.loc[df['hemi'] == 'R']
+        print(dfL)
 
         # plt.figure()
         fig,ax=plt.subplots(nrows=2, figsize=(10,8))
@@ -23,6 +29,11 @@ for alpha in alpha_values:
         fig.tight_layout()
         fig.set_figheight(9)
         fig.set_figwidth(15)
+
+        # Folder for results of this analysis
+        folder = f'classifier_results_alpha-{alpha}_l1ratio-{l1_ratio}'
+        os.makedirs(os.path.join(analysis_root, folder), exist_ok=True) # Make folder if it doesn't already exist
+
 
         plt.savefig((f'res_alpha_{alpha}_l1_ratio_{l1_ratio}.png'), bbox_inches='tight')
         print(f'Figure saved as res_alpha_{alpha}_l1_ratio_{l1_ratio}.png')
