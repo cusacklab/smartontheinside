36146994c1a6b3e4afbdecf0bf825a2786b5621d
chiaracc <chiaracaldinelli@gmail.com>
2022-11-19 23:21:07 +0000

    right figures

--- classifier_check_res.py
+++ classifier_check_res.py
@@ -53,13 +53,13 @@ for hemiind, hemi in enumerate(['R', 'L']):
             
         # Compose figure
         # im = ax[taskind][hemiind].imshow(m)
-        ax[taskind][hemiind].imshow(m)
+        im = ax[taskind][hemiind].imshow(m)
 
-        # # Show all ticks and label them with the respective list entries
-        ax[taskind][hemiind].set_yticks(np.arange(len(alpha_values)), labels=alpha_values)
-        ax[taskind][hemiind].set_xticks(np.arange(len(l1_ratio_values)), labels=l1_ratio_values)
-        plt.xlabel('l1 ratio')
-        plt.ylabel('alpha')
+        # # Show all ticks and label them with the respective list entries 
+        ax[taskind][hemiind].set_yticks(np.arange(len(alpha_values)), labels=alpha_values, fontsize=5)
+        ax[taskind][hemiind].set_xticks(np.arange(len(l1_ratio_values)), labels=l1_ratio_values, fontsize=5)
+        plt.xlabel('l1 ratio', fontsize=5)
+        plt.ylabel('alpha', fontsize=5)
 
         # Rotate the tick labels and set their alignment.
         plt.setp(ax[taskind][hemiind].get_xticklabels(), rotation=45, ha="right",
@@ -67,18 +67,18 @@ for hemiind, hemi in enumerate(['R', 'L']):
 
         # create an Axes on the right side of ax. The width of cax will be 5%
         # of ax and the padding between cax and ax will be fixed at 0.05 inch.
-        # divider = make_axes_locatable(ax[hemiind][taskind])
+        # divider = make_axes_locatable(ax[taskind][hemiind])
         # cax = divider.append_axes("right", size="5%", pad=0.05)
         # plt.colorbar(im, cax=cax)
         # plt.tight_layout()
 
         plt.show()
 
-        ax[taskind][hemiind].set_title(f"{hemi} Hemi - {task}")
+        ax[taskind][hemiind].set_title(f"{hemi} Hemi - {task}", fontsize=8)
         fig.tight_layout()
         # plt.show()
 
-plt.savefig(os.path.join(analysis_root, folder, f'heatmap.png'), bbox_inches='tight')
+plt.savefig(os.path.join(analysis_root, folder, f'heatmap_without_colourbar.png'), bbox_inches='tight')
 print(f'Figure saved as heatmap.png')
 
 # plt.savefig(os.path.join(analysis_root, folder, f'heatmap_alpha-{alpha}_l1_ratio-{l1_ratio}.png'), bbox_inches='tight')
