81b3468b196e94259851aa05af78b8807e4422a4
chiaracc <chiaracaldinelli@gmail.com>
2022-11-19 22:41:42 +0000

    one figure with all res

--- classifier_check_res.py
+++ classifier_check_res.py
@@ -21,81 +21,64 @@ analysis_root = '/home/chiaracaldinelli'
 m = np.zeros(((len(alpha_values)),(len(l1_ratio_values))))
 
 
-for alphaind, alpha in enumerate(alpha_values):
-    for l1_ratioind, l1_ratio in enumerate(l1_ratio_values):
+# Make composite figures
+plt.figure()
+fig,ax=plt.subplots(ncols=2, nrows=5, figsize=(10,8))
 
-        plt.figure()
-        fig,ax=plt.subplots(ncols=2, nrows=5, figsize=(10,8))
+for hemiind, hemi in enumerate(['R', 'L']):
+    for taskind, task in enumerate(tasks_selected):
 
-        for hemiind, hemi in enumerate(['R', 'L']):
-            # Make composite figures
-            
-
-            for taskind, task in enumerate(tasks_selected):
+        for alphaind, alpha in enumerate(alpha_values):
+            for l1_ratioind, l1_ratio in enumerate(l1_ratio_values):
 
                 folder = f'smartontheinside/smartontheinside/results/classifier_tune_parameters'
                 os.makedirs(os.path.join(analysis_root, folder), exist_ok=True) # Make folder if it doesn't already exist
 
                 df = pd.read_csv(os.path.join(analysis_root, f'classifier_results_alpha-{alpha}_l1ratio-{l1_ratio}/summary_N-20.csv'), index_col=False)
                 df = df.loc[df['hemi'] == hemi]
-                # print(dfL)
-                print(df)
                 table = pd.pivot_table(df, values=['pearson', 'score'], index=['hemi', 'task'],
                     aggfunc={'pearson': np.mean,
                              'score': np.mean})
+                # print(table)
                 
                 # file = open(os.path.join(analysis_root, folder, f'classifier_res.txt'),'a')
                 # file.write(f"\n Results for alpha = {alpha} and l1_ratio = {l1_ratio}")
                 # file.write(f"\n {table}")
                 # file.close()
 
-                print(table)
-
-                # plt.figure()
-                # fig,ax=plt.subplots(nrows=2, figsize=(10,8))
-                # fig=interaction_plot(x=dfR['fold'], trace=dfR['task'], response=dfR['score'], ax=ax[0])
-                # fig=interaction_plot(x=dfR['fold'], trace=dfR['task'], response=dfR['score'], ax=ax[1])
-                # fig.tight_layout()
-                # fig.set_figheight(9)
-                # fig.set_figwidth(15)
-
-                # # Folder for results of this analysis
-                # folder = f'classifier_results_alpha-{alpha}_l1ratio-{l1_ratio}'
-                # os.makedirs(os.path.join(analysis_root, folder), exist_ok=True) # Make folder if it doesn't already exist
-
-                # plt.savefig((f'res_alpha_{alpha}_l1_ratio_{l1_ratio}.png'), bbox_inches='tight')
-                # print(f'Figure saved as res_alpha_{alpha}_l1_ratio_{l1_ratio}.png')
 
                 # Calculate average of the 20 folds and summarise it in a matrix
                 y = (df.loc[df['task'] == task]['score'])
                 m[alphaind,l1_ratioind] = y.mean()
             
-                # fig, ax = plt.subplots()
-                # Compose figure
-                im = ax[taskind][hemiind].imshow(m)
-                # im = ax.imshow(m)
+        # Compose figure
+        # im = ax[taskind][hemiind].imshow(m)
+        ax[taskind][hemiind].imshow(m)
 
-                # # Show all ticks and label them with the respective list entries
-                ax[taskind][hemiind].set_xticks(np.arange(len(alpha_values)), labels=alpha_values)
-                ax[taskind][hemiind].set_yticks(np.arange(len(l1_ratio_values)), labels=l1_ratio_values)
-                plt.xlabel('l1 ratio')
-                plt.ylabel('alpha')
-                # Rotate the tick labels and set their alignment.
-                plt.setp(ax[taskind][hemiind].get_xticklabels(), rotation=45, ha="right",
-                        rotation_mode="anchor")
+        # # Show all ticks and label them with the respective list entries
+        ax[taskind][hemiind].set_yticks(np.arange(len(alpha_values)), labels=alpha_values)
+        ax[taskind][hemiind].set_xticks(np.arange(len(l1_ratio_values)), labels=l1_ratio_values)
+        plt.xlabel('l1 ratio')
+        plt.ylabel('alpha')
 
-                # create an Axes on the right side of ax. The width of cax will be 5%
-                # of ax and the padding between cax and ax will be fixed at 0.05 inch.
-                # divider = make_axes_locatable(ax[hemiind][taskind])
-                # cax = divider.append_axes("right", size="5%", pad=0.05)
-                # plt.colorbar(im, cax=cax)
-                # plt.tight_layout()
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax[taskind][hemiind].get_xticklabels(), rotation=45, ha="right",
+                rotation_mode="anchor")
 
-            
-                plt.show()
+        # create an Axes on the right side of ax. The width of cax will be 5%
+        # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+        # divider = make_axes_locatable(ax[hemiind][taskind])
+        # cax = divider.append_axes("right", size="5%", pad=0.05)
+        # plt.colorbar(im, cax=cax)
+        # plt.tight_layout()
 
-                ax[taskind][hemiind].set_title(f"{hemi} Hemi - {task}")
-                fig.tight_layout()
-                # plt.show()
-        # plt.savefig(os.path.join(analysis_root, folder, f'heatmap_{hemi}_{task}'), bbox_inches='tight')
-        plt.savefig(os.path.join(analysis_root, folder, f'heatmap_alpha-{alpha}_l1_ratio-{l1_ratio}.png'), bbox_inches='tight')
+        plt.show()
+
+        ax[taskind][hemiind].set_title(f"{hemi} Hemi - {task}")
+        fig.tight_layout()
+        # plt.show()
+
+plt.savefig(os.path.join(analysis_root, folder, f'heatmap.png'), bbox_inches='tight')
+print(f'Figure saved as heatmap.png')
+
+# plt.savefig(os.path.join(analysis_root, folder, f'heatmap_alpha-{alpha}_l1_ratio-{l1_ratio}.png'), bbox_inches='tight')
